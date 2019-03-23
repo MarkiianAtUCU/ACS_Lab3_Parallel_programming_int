00a6f736e9bd3327787589c2e39f3b21c96da215
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2019-03-23 14:57:42 +0200

    Update Python script

--- start.py
+++ start.py
@@ -4,21 +4,25 @@ import sys
 
 SINGLE_THREAD_PATH = "./build/prime_numbers"+".exe" if os.name == 'nt' else ""
 MULTI_THREAD_PATH  = "./build/prime_numbers_single_thread"+".exe" if os.name == 'nt' else ""
+THREADS = [4, 8, 16, 32, 64, 256, 512]
 
 
 def single_thread(number_of_iterations):
     for i in range(number_of_iterations):
-        subprocess.run(SINGLE_THREAD_PATH)
+        subprocess.run([SINGLE_THREAD_PATH])
 
 
 def multi_thread(number_of_iterations, number_of_threads):
     for i in range(number_of_iterations):
-        subprocess.run(MULTI_THREAD_PATH)
+        subprocess.run([MULTI_THREAD_PATH, number_of_threads])
 
 
 def main(number_of_iterations):
-    pass
+    single_thread(number_of_iterations)
+
+    for thread in THREADS:
+        multi_thread(number_of_iterations, thread)
 
 
 if __name__ == "__main__":
-    main(100)
+    main(1)
